2be8198e3525f78ebd2a0f8fd6ce295efdd901ef
동윤 강 <davidkang8111@gmail.com>
2023-03-09 15:12:34 +0900

    202303091507 깃허브 연동 테스트

---
+++ IfElse/MainApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElse
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GitHub 테스트 중입니다~");
+        }
+    }
+}
