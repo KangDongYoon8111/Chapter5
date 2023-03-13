da6e86d00336ec617ee5cdd9a3c0d66d06282daf
동윤 강 <davidkang8111@gmail.com>
2023-03-13 15:38:42 +0900

    202303131538

---
+++ p151_ex2/MainApp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p151_ex2
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("점수를 입력하세요. : ");
+
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+
+            string grade;
+            if (number >= 90)
+            {
+                grade = "A";
+            }
+            else if (number >= 80)
+            {
+                grade = "B";
+            }
+            else if (number >= 70)
+            {
+                grade = "C";
+            }
+            else
+            {
+                grade = "F";
+            }
+
+            Console.WriteLine($"입력하신 점수 {number}는(은) " +
+                $"{grade} 학점 입니다.");
+        }
+    }
+}
---
+++ p151_ex3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p151_ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("숫자를 입력하세요. : ");
+
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+
+            //if(number >= 50)
+            //{
+            //    Console.WriteLine($"입력하신 숫자 {number}는(은) 50 이상의 수 입니다.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"입력하신 숫자 {number}는(은) 50 미만의 수 입니다.");
+            //}
+            Console.WriteLine("입력하신 숫자 {0}는(은) {1} 입니다.", number, number >= 50 ? "50이상의 수" : "50미만의 수");
+        }
+    }
+}
